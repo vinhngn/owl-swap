cdafe7abd46d22a8d86a04002a0bcec0a4f7122e
vinhngn <vinhn.official@gmail.com>
2024-10-28 15:18:53 +0900

    Update JwtService

--- backend/src/main/java/com/example/backend/dto/UserDto.java
+++ backend/src/main/java/com/example/backend/dto/UserDto.java
@@ -1,8 +1,5 @@
 package com.example.backend.dto;
 
-
-import java.util.Date;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +7,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
-    private String userId;
-    private String fullName;
+    private String userName;
     private String email;
     private Boolean isActive;
-    private Date createdAt;
-    private Date updatedAt;
-    
 }
