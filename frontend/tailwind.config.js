334060a0fb1e8a7af5238d7a9de4fdd94f568984
vinhngn <vinhn.official@gmail.com>
2024-11-02 23:19:40 +0900

    fixing

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 // tailwind.config.js
 module.exports = {
-    content: [
-      "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust this path to your project structure
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust this path to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
